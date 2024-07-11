fc3307e19ed6549dd33c203fafa0ea94b6ac1bb5
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-11 17:48:45 +0200

    Exploring Variables

--- app/src/main/java/com/example/floatingtypes/FloatingTypes.kt
+++ app/src/main/java/com/example/floatingtypes/FloatingTypes.kt
@@ -16,4 +16,20 @@ fun main(){
 // false
     println(boolNull)
 // null
+
+    //Tra apici singoli è un solo carattere
+    val aChar = 'a'
+
+    println(aChar)
+    println('\n') // Prints an extra newline character
+    println('\u00AE')
+
+    var str = "abcd 123"
+    str = "Giacomo"
+    //equivalente a json
+    println(str.lowercase())
+    //concatenare
+    println(str.lowercase() +" "+ "marongiu")
+    println(str.lowercase() +"\n"+ "marongiu")
+
 }
