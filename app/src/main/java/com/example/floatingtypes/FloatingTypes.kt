fb0d6ee18b14b8114e29253d85da9918db9d4ab6
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-07-11 17:20:25 +0200

    Variables

--- app/src/main/java/com/example/floatingtypes/FloatingTypes.kt
+++ app/src/main/java/com/example/floatingtypes/FloatingTypes.kt
@@ -3,4 +3,17 @@ package com.example.floatingtypes
 fun main(){
     val circleRatio = 3.14
     println(circleRatio)
+
+    val myTrue: Boolean = true
+    val myFalse: Boolean = false
+    val boolNull: Boolean? = null
+
+    println(myTrue || myFalse)
+// true
+    println(myTrue && myFalse)
+// false
+    println(!myTrue)
+// false
+    println(boolNull)
+// null
 }
